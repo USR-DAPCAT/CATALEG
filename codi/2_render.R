c69c37d1a44c8935caa07885009b4130c5402ef7
Ramon Puig Treserres <raypuig@gmail.com>
2022-05-30 17:27:25 +0200

    act

--- codi/2_render.R
+++ codi/2_render.R
@@ -1,6 +1,4 @@
-#[30.V.2022]#                                                 
-##C A T A L E G#
-
+#C A T A L E G#:: [30.V.2022]
 #----------------------------------------------------------------------------------------# 
 library("dplyr")
 rm(list=ls())
@@ -8,17 +6,12 @@ gc()
 #
 dir_dades_origen="../../DADES/CATALEG" 
 dir_dades_origen_ACTUALITZACIO="../../../DADES/CATALEG/Cataleg_ORIGINAL_temp.xlsx"
-
-
-
 # Aquetsa part, compararem el nostre CATALEG_PROJECTE amb el CATALEG_ORIGINAL, i ens dirà amb un Excel[Codis_Invest.xlsx] quants CODIS(cod) falten al CATALEG_ORIGINAL.
 #--------------------------------------------------------------------------------------------------#
 rmarkdown::render(input="./codi/1_lectura1.Rmd",
                   output_file=paste0(here::here("resultats/Informe_Lectura_Cataleg1_"),Sys.Date()),
                   params = list(dir_dades_origen=dir_dades_origen))
-
-
-
+#
 # Aquesta II part,rebrem del investigador els codis corregits i els afegirem al CATALEG ORIGINAL [actualiutazcio CATALEG ORIGINAL], 
 # despres agafarem , nomes els codis del projecte, perque l investigador hi posi els AGR1.  
 #--------------------------------------------------------------------------------------------------#
